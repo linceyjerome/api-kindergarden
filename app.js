1dba60a3ed32b12a3693956fa1c1c513a9a39997
Lincey Jerome <lj_jerome@hotmail.com>
2023-04-19 23:40:17 -0400

    add asyc await

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
 const express = require('express');
 const data = require('./data.ts');
-
+const logger = require('pino-http');
 const app = express();
 
-app.get('/api/info/:id', (req, res) => {
-  const { id } = req.params;
-  const info = data.info[0].find((item) => item.id === id);
-  console.log(data.info.length)
-  if (info) {
-    return res.status(200).json(info);
-  } else {
-    return res
-      .status(404)
-      .json({ message: `item with id: ${id} not found. Check data` });
+// GET info by ID
+app.get('/api/info/:id', async (req, res) => {
+  try {
+    logger(req, res);
+
+    const { id } = req.params;
+    const info = await data.info[0].find((item) => item.id === id);
+    if (info) {
+      return res.status(200).json(info);
+    } else {
+      return res.status(204).end();
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: 'Internal server error' });
   }
 });
 
-app.get('/api/info', (req, res) => {
-  const info = data.info[0];
-
-  if (info.length > 0) {
-    return res.status(200).json(info);
-  } else {
-    return res
-      .status(404)
-      .json({ message: `No items found. Check data` });
+// GET all info
+app.get('/api/info', async (_req, res) => {
+  try {
+    logger(_req, res);
+    const info = await data.info[0];
+    if (info.length > 0) {
+      return res.status(200).json(info);
+    } else {
+      return res.status(204).end();
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: 'Internal server error' });
   }
 });
-
 
 app.listen(5000, () => {
+
   console.log('Server is running on port 5000');
 });
